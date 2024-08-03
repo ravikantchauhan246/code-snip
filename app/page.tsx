ac3eb9bad8c8674c0b93af8e03d74d1cee42b3c8
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-08-03 12:00:43 +0530

    complete call to action

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home(){
   return(
     <div className="poppins">
       <Navbar/>
+      <CTASection/>
     </div>
   )
 
@@ -22,11 +23,11 @@ function Navbar(){
 function Logo(){
   return(
     <div className="flex gap-2 items-center">
-      <div className={`bg-[${mainColor}] p-[3px] rounded-md`}>
+      <div style={{backgroundColor: mainColor}} className={`p-[3px] rounded-md`}>
         <TextSnippetIcon sx={{fontSize:40,color:"white"}}/>
       </div>
       <div className="flex gap-1 text-[19px]">
-        <span className={`font-bold text-[${mainColor}]`}>Code</span>
+        <span style={{color:mainColor}} className={`font-bold`}>Code</span>
         <span className="text-slate-600">Snip</span>
       </div>
     </div>
@@ -36,12 +37,33 @@ function Logo(){
 function Button(){
   return(
     <div className="flex gap-2 max-sm:flex-col max-sm:w-[60%] max-sm:mt-8">
-      <button className={`max-sm:w-full bg-[${mainColor}] p-[8px] px-6 text-sm text-white rounded-md`}>
+      <button style={{backgroundColor:mainColor}} className={`max-sm:w-full p-[8px] px-6 text-sm text-white rounded-md`}>
         Sign In
       </button>
-      <button className={`text-sm border border-[${mainColor}] text-[${mainColor}]\ hover:bg-[${mainColor}] hover:text-white p-[8px] px-6 rounded-md`}>
+      <button style={{borderColor:mainColor, color:mainColor,}} className={`text-sm border border-[${mainColor}] text-[${mainColor}]\ hover:bg-[${mainColor}] hover:text-white p-[8px] px-6 rounded-md`}>
         Sign Up
       </button>
     </div>
   )
 }
+
+function CTASection(){
+  return(
+    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6">
+      <h2 className="font-bold text-2xl text-center">
+        Organize Your Code Snippets
+        <span style={{color:mainColor}} > Efficiently</span>
+      </h2>
+      <p className="text-center text-sm w-[450px] max-sm:w-full text-slate-500">
+        With our advanced tagging and search features,you can quickly find the 
+        snippet you need,right when you need it. Spend less time searching for code and more time writing it.
+      </p>
+
+      <button className={`block px-9 py-3 text-sm font-medium text-white transition focus:outline-none`}
+      type="button">
+        {`Let's get started!`}
+      </button>
+    </div>
+  );
+}
+
